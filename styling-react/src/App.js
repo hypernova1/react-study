29fbbddf403ca42275501642997ffd725d479c41
hypernova1 <chtlstjd01@gmail.com>
2018-12-23 00:34:01 +0900

    20181222

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import classNames from 'classnames'
-import classnames from 'classnames/bind'
-import styles from './App.module.css';
+import StyledButton from './component/StyedButton'
+// import classNames from 'classnames'
+// import classnames from 'classnames/bind'
+// import styles from './App.module.css';
+// import styles from './App.scss'
 
-const cx = classnames.bind(styles);
+// const isBlue = true;
+// const cx = classnames.bind(styles);
+
+// 일반 css
+// <div className={[styles.box, styles.blue].join(' ')}></div>
+// classnames 라이브러리 사용
+// <div className={ classNames(styles.box, styles.blue) }></div>
+// classnames/bind 사용
+// <div className={cx('box', {blue: isBlue})}>bbb</div>
+
+// Sass
+// <div className={cx('box', {blue: isBlue})}>
+//   <div className={cx('box-inside')}/>
+// </div> 
+
+
 class App extends Component {
   render() {
     return (
-      <>
-      <div className={[styles.box, styles.blue].join(' ')}></div>
-      {/* classnames 라이브러리 사용 */}
-      <div className={ classNames(styles.box, styles.blue) }></div>
-      {/* classnames/bind 사용 */}
-      <div className={cx('box', 'blue')}>aaa</div>
-      </>
+      <div>
+        <StyledButton>버튼</StyledButton>
+      </div>
     );
   }
 }
